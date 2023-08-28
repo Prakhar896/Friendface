d31ea3f79bdf6609823e2e982adcb0be3e6d920d
Prakhar Trivedi <Prakhar896@users.noreply.github.com>
2023-08-28 08:56:01 +0800

    Initial Commit

---
+++ Friendface/FriendfaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  FriendfaceApp.swift
+//  Friendface
+//
+//  Created by Prakhar Trivedi on 28/8/23.
+//
+
+import SwiftUI
+
+@main
+struct FriendfaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
